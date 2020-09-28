296e5e685e3d8f552f333226662a92965e933a91
v2thegreat <v2thegreat@gmail.com>
2020-09-28 19:44:26 -0400

    Added preprocessor function for csv and json + other formats

--- preprocess.py
+++ preprocess.py
@@ -3,9 +3,12 @@ import tarfile
 import time
 import zipfile
 from datetime import timedelta
+from morpheus.data_loading import DataLoading
 
 
 class Preprocess:
+    _output_formats = ['csv', 'json', 'parquet', 'sql']
+
     def __init__(self):
         os.chdir('..')
         if os.path.isdir("raw"):
@@ -56,7 +59,48 @@ class Preprocess:
             else:
                 RuntimeError(f"File extension {file_extension} not recognized.")
         if verbose:
-            print(f"\nSucessfully extracted all files in {directory_path} to {destination}")
+            print(f"\nSuccessfully extracted all files in {directory_path} to {destination}")
+
+    @staticmethod
+    def store_tweets_to_file_format(directory_path: str, destination: str, suffix: str = '*.json*',
+                                    recursive: bool = True,
+                                    output_format='csv'):
+        """
+        Extracting all tweets and storing them as csv.
+        This function recursively searches the `directory_path` for files that match the given `suffix`
+
+        :param directory_path: path of the directory where the data is stored.
+        :param suffix: suffix: suffix to search for files with
+        :param destination: where the output needs to be stored
+        :param recursive: recursively search for files?
+        :param output_format: the format that the data should be stored in.
+                              Possible values are:  ['csv', 'json', 'parquet', 'sql']
+        :return:
+        """
+
+        if output_format not in Preprocess._output_formats:
+            raise ValueError(f'The given format: {output_format} is not in {Preprocess._output_formats}')
+
+        files_list = DataLoading.get_files_list(pathname=directory_path, suffix=suffix, recursive=recursive)
+        data = DataLoading.get_twitter_data_from_file_list(file_lst=files_list)
+        data = data.to_dataframe()
+
+        if output_format == 'csv':
+            data.to_csv(destination)
+
+        elif output_format == 'json':
+            data.to_json(destination)
+
+        elif output_format == 'parquet':
+            data.to_parquet(destination)
+
+        elif output_format == 'sql':
+            raise NotImplementedError('SQL has not been yet implemented. '
+                                      'If you would like this feature, please make an issue here: '
+                                      'https://github.com/Twitter-Public-Analysis/Twitter-Public-Analysis/issues')
+
+        else:
+            raise ValueError(f'The given format: {output_format} is not recognized')
 
 
 if __name__ == "__main__":
