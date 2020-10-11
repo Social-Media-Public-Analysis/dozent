df018b21fdce7ecb16a6de55576e593de947ede7
v2thegreat <v2thegreat@gmail.com>
2020-10-10 23:44:09 -0400

    Removed command_line_utils.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,5 @@ from .dozent import Dozent
 from .catch_thread_exceptions import install as _catch_thread_exceptions
 
 _catch_thread_exceptions()
-from .command_line_utils import text_renderer
 
-__all__ = ['Dozent', 'text_renderer']
+__all__ = ['Dozent', ]
