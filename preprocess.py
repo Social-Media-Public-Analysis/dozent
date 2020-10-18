617285cc8e9dae90d68f6c528db03af2d9373b54
v2thegreat <v2thegreat@gmail.com>
2020-10-18 13:21:26 -0400

    Added functionality to resolve issue #25 and added tests to test that function out. Also added support for unsupported formats that we might encounter

--- preprocess.py
+++ preprocess.py
@@ -4,10 +4,14 @@ import time
 import zipfile
 from datetime import timedelta
 from morpheus.data_loading import DataLoading
+from pathlib import Path
+from typing import Union, List
 
 
 class Preprocess:
     _output_formats = ['csv', 'json', 'parquet', 'sql']
+    _supported_formats = ['csv', 'json']
+    _unsupported_formats = ['parquet', 'sql']
 
     def __init__(self):
         os.chdir('..')
@@ -17,25 +21,25 @@ class Preprocess:
             os.mkdir('raw')
 
     @staticmethod
-    def _untar_file(file_path: str, dest: str):
+    def _untar_file(file_path: Union[str, Path], destination: Union[str, Path]):
         """
         Untars a single tar file into target directory
         :return: None
         """
         file_path = tarfile.open(file_path)
-        file_path.extract(dest)
+        file_path.extract(destination)
 
     @staticmethod
-    def _unzip_file(file_path: str, dest: str):
+    def _unzip_file(file_path: Union[str, Path], destination: Union[str, Path]):
         """
         Unzips a single zip file into target directory
         :return: None
         """
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(dest)
+            zip_ref.extractall(destination)
 
     @staticmethod
-    def extract_directory(directory_path: str, destination: str, verbose: bool = True):
+    def extract_directory(directory_path: Union[str, Path], destination: Union[str, Path], verbose: bool = True):
         """
         Extracts all files in a directory into target directory
         :param directory_path: path of the directory
@@ -62,9 +66,10 @@ class Preprocess:
             print(f"\nSuccessfully extracted all files in {directory_path} to {destination}")
 
     @staticmethod
-    def store_tweets_to_file_format(directory_path: str, destination: str, suffix: str = '*.json*',
+    def store_tweets_to_file_format(directory_path: Union[str, Path], destination: Union[str, Path],
+                                    suffix: str = '*.json*',
                                     recursive: bool = True,
-                                    output_format='csv'):
+                                    output_format='csv') -> List[str]:
         """
         Extracting all tweets and storing them as csv.
         This function recursively searches the `directory_path` for files that match the given `suffix`
@@ -74,27 +79,30 @@ class Preprocess:
         :param destination: where the output needs to be stored
         :param recursive: recursively search for files?
         :param output_format: the format that the data should be stored in.
-                              Possible values are:  ['csv', 'json', 'parquet', 'sql']
-        :return:
+                              While the possible values are:  ['csv', 'json', 'parquet', 'sql'], we currently test for
+                              only ['csv', 'json']
+        :return: returns the list of all of the output files
         """
-
-        if output_format not in Preprocess._output_formats:
-            raise ValueError(f'The given format: {output_format} is not in {Preprocess._output_formats}')
+        if output_format not in Preprocess._supported_formats:
+            raise ValueError(f'The given format: {output_format} is not in {Preprocess._supported_formats}')
 
         files_list = DataLoading.get_files_list(pathname=directory_path, suffix=suffix, recursive=recursive)
-        data = DataLoading.get_twitter_data_from_file_list(file_lst=files_list)
+        data = DataLoading.get_twitter_data_from_file_list(file_lst=files_list, remove_deleted_tweets=True)
         data = data.to_dataframe()
 
         if output_format == 'csv':
-            data.to_csv(destination)
+            return data.to_csv(destination)
 
         elif output_format == 'json':
-            data.to_json(destination)
+            return data.to_json(destination)
 
-        elif output_format == 'parquet':
-            data.to_parquet(destination)
+        elif output_format == 'parquet':  # skip_tests
+            raise NotImplementedError('Parquet has not been yet supported because of the added dependency of '
+                                      'fastparque/pyarrow, which we might look into later'
+                                      'If you would like this feature, please make an issue here: '
+                                      'https://github.com/Twitter-Public-Analysis/Twitter-Public-Analysis/issues')
 
-        elif output_format == 'sql':
+        elif output_format == 'sql':  # skip_tests
             raise NotImplementedError('SQL has not been yet implemented. '
                                       'If you would like this feature, please make an issue here: '
                                       'https://github.com/Twitter-Public-Analysis/Twitter-Public-Analysis/issues')
@@ -104,6 +112,4 @@ class Preprocess:
 
 
 if __name__ == "__main__":
-    _start_time = time.time()
-    Preprocess.extract_directory('dozent/data', 'preprocess/raw')
-    print(f"\nElasped Time: {timedelta(seconds=(time.time() - _start_time))}")
+    pass
