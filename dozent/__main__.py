ef6b3474048167c94b6699c0d1dcdf0222e0b40d
Eric Burt <eric.burt@uconn.edu>
2021-01-25 01:38:59 -0500

    dozent/__main__.py
    Adds testing argument to CLI

--- dozent/__main__.py
+++ dozent/__main__.py
@@ -10,33 +10,71 @@ except ModuleNotFoundError:
     from dozent import Dozent
 
 CURRENT_FILE_PATH = Path(__file__)
-DEFAULT_DATA_DIRECTORY = CURRENT_FILE_PATH.parent.parent / 'data'
+DEFAULT_DATA_DIRECTORY = CURRENT_FILE_PATH.parent.parent / "data"
 
 
 def main(command_line_arguments: Dict[str, Any]):  # skip_tests
     _start_time = time.time()
-    verbose = not command_line_arguments['quiet']
+    verbose = not command_line_arguments["quiet"]
     _dozent_object = Dozent()
 
-    _dozent_object.download_timeframe(start_date=command_line_arguments['start_date'],
-                                      end_date=command_line_arguments['end_date'],
-                                      verbose=verbose)
+    if command_line_arguments["start_date"] and command_line_arguments["end_date"]:
+        _dozent_object.download_timeframe(
+            start_date=command_line_arguments["start_date"],
+            end_date=command_line_arguments["end_date"],
+            verbose=verbose,
+        )
 
-    if command_line_arguments['timeit']:
-        print(f"Download Time: {datetime.timedelta(seconds=(time.time() - _start_time))}")
+        if command_line_arguments["timeit"]:
+            print(
+                f"Download Time: {datetime.timedelta(seconds=(time.time() - _start_time))}"
+            )
+
+    elif command_line_arguments["test"]:
+        #_dozent_object.download_test()
+
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='A powerful downloader to get tweets from twitter for our compute. '
-                                                 'The first step of many')
-    parser.add_argument('-s', '--start-date', help="The date from where we download. The format must be: YYYY-MM-DD",
-                        required=True, type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d').date())
-    parser.add_argument('-e', '--end-date', help="The last day that we download. The format must be: YYYY-MM-DD",
-                        required=True, type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d').date())
-    parser.add_argument('-t', '--timeit', help='Show total program runtime', default=True)
-    parser.add_argument('-o', '--output-directory', help='Output Directory where the file will be stored. '
-                                                         'Defaults to the data/ directory',
-                        default=DEFAULT_DATA_DIRECTORY)
-    parser.add_argument('-q', '--quiet', help='Turn off output (except for errors and warnings)', action='store_true')
+    parser = argparse.ArgumentParser(
+        description="A powerful downloader to get tweets from twitter for our compute. "
+        "The first step of many"
+    )
+    parser.add_argument(
+        "-s",
+        "--start-date",
+        help="The date from where we download. The format must be: YYYY-MM-DD",
+        type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d").date(),
+    )
+    parser.add_argument(
+        "-e",
+        "--end-date",
+        help="The last day that we download. The format must be: YYYY-MM-DD",
+        type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d").date(),
+    )
+    parser.add_argument(
+        "-t", "--timeit", help="Show total program runtime", default=True
+    )
+    parser.add_argument(
+        "-o",
+        "--output-directory",
+        help="Output Directory where the file will be stored. "
+        "Defaults to the data/ directory",
+        default=DEFAULT_DATA_DIRECTORY,
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        help="Turn off output (except for errors and warnings)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-p",
+        "--test",
+        help="Downloads 4 small files for testing purposes (50 MB total)",
+        action="store_true",
+    )
     args = parser.parse_args()
     main(vars(args))
