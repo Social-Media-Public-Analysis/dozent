f82369d584f9a027a9c8e9d74d4f1c648b45525e
Eric Burt <eric.burt@uconn.edu>
2021-01-28 18:39:09 -0500

    Track global final download size

--- dozent/downloader_tools.py
+++ dozent/downloader_tools.py
@@ -3,13 +3,20 @@ import sys
 from typing import Tuple
 from threading import Lock
 from pySmartDL import SmartDL
+
 # Used for tracking and displaying download progress bars
 global global_progress_tracker
 global_progress_tracker = [(0, 0, "0", 0, "0")]
 global global_download_size
 global_download_size = 0
+
+global global_final_download_size
+global_final_download_size = 0
+
+
 class DownloaderTools:
     __instance__ = None
+
     def __init__(self):
         if DownloaderTools.__instance__ is None:
             DownloaderTools.__instance__ = self
@@ -17,9 +24,10 @@ class DownloaderTools:
             raise RuntimeError(
                 f"Singleton {self.__class__.__name__} class is created more than once!"
             )
+
     @staticmethod
     def _get_individual_download_stats(
-            downloader_obj: SmartDL,
+        downloader_obj: SmartDL,
     ) -> Tuple[int, int, str, float, str]:
         download_size = downloader_obj.get_dl_size() >> 20
         file_size = downloader_obj.get_final_filesize() >> 20
@@ -27,25 +35,28 @@ class DownloaderTools:
         progress_percentage = downloader_obj.get_progress()
         eta = downloader_obj.get_eta()
         return download_size, file_size, speed, progress_percentage, eta
+
     @staticmethod
     def _update_global_download_size() -> int:
         global global_download_size
         for download in global_progress_tracker:
             global_download_size += download[0]
+
     @staticmethod
     def _get_final_download_size() -> int:
         global_final_download_size = 0
         for download in global_progress_tracker:
             global_final_download_size += download[1]
         return global_final_download_size
+
     @classmethod
     def download_with_pysmartdl(
-            cls,
-            link: str,
-            download_dir: str,
-            task_id: int,
-            number_of_dates: int,
-            verbose: str = True,
+        cls,
+        link: str,
+        download_dir: str,
+        task_id: int,
+        number_of_dates: int,
+        verbose: str = True,
     ):
         """
         Downloads file from link using PySmartDL
@@ -57,6 +68,10 @@ class DownloaderTools:
             global_progress_tracker = global_progress_tracker * number_of_dates
         downloader_obj = SmartDL(link, download_dir, progress_bar=False)
         downloader_obj.start(blocking=False)
+
+        global global_final_download_size
+        global_final_download_size += downloader_obj.get_final_filesize()
+
         while not downloader_obj.isFinished():
             if verbose:
                 lock = Lock()
@@ -65,8 +80,10 @@ class DownloaderTools:
                     downloader_obj
                 )
                 cls._update_global_download_size()
-                sys.stdout.write(f"{global_download_size} \r")
+                sys.stdout.write(
+                    f"{global_download_size} Bytes / {global_final_download_size} Bytes    \r"
+                )
                 sys.stdout.flush()
                 lock.release()
             # Sleep for a random interval between 0.01 and 0.25 seconds
-            time.sleep(.25)
+            time.sleep(0.25)
