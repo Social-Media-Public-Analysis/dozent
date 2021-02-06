ad3d68c496d9867a5776e92c4ecd4bab61431c14
Eric Burt <eric.burt@uconn.edu>
2021-02-06 10:26:33 -0800

    Update test_download_with_pysmartdl

--- tests/test_downloader_tools.py
+++ tests/test_downloader_tools.py
@@ -31,7 +31,8 @@ class DownloaderToolsTestCase(unittest.TestCase):
 
         DownloaderTools.download_with_pysmartdl(link='http://ipv4.download.thinkbroadband.com/20MB.zip',
                                                 download_dir='test_downloader_dir',
-                                                task_id=0
+                                                task_id=0,
+                                                number_of_dates=1
                                                 )
 
         self.assertTrue(exists('test_downloader_dir/20MB.zip'))
